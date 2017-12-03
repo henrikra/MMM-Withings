0148d6b98d2e373f4b5450335a713959b7734b9b
Henrik Raitasola <henrik.raitasola@gmail.com>
2017-12-04 01:18:43 +0200

    Add localization

--- MMM-Withings.js
+++ MMM-Withings.js
@@ -9,6 +9,13 @@ Module.register('MMM-Withings', {
     return ['moment.js'];
   },
 
+  getTranslations: function() {
+    return {
+      en: 'translations/en.json',
+      fi: 'translations/fi.json',
+    }
+  },
+
   setState: function(newData, animationSpeed) {
     this.state = Object.assign({}, this.state, newData);
     this.updateDom(animationSpeed);
@@ -27,9 +34,9 @@ Module.register('MMM-Withings', {
     const upperText = document.createElement('div');
     const lowerText = document.createElement('div');
     upperText.classList.add('small');
-    upperText.innerHTML = this.state.weight ? `${moment(this.state.date * 1000).fromNow()} you were` : '';
+    upperText.innerHTML = this.state.weight ? this.translate('agoYouWere', {agoTime: moment(this.state.date * 1000).fromNow()}) : '';
     lowerText.classList.add('medium');
-    lowerText.innerHTML = this.state.weight ? this.formatWeight() : 'Loading...';
+    lowerText.innerHTML = this.state.weight ? this.formatWeight() : this.translate('loading');
     wrapper.appendChild(upperText);
     wrapper.appendChild(lowerText);
     return wrapper;
