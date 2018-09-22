bf75b955c2159c98e81d5e7b9981a6a9f8855bd3
Henrik Raitasola <henrik.raitasola@gmail.com>
2018-09-22 15:28:05 +0300

    Start to use the new api

--- README.md
+++ README.md
@@ -2,7 +2,4 @@
 
 | Key           | Type   | Required |
 | ------------- | ------ | -------- |
-| apiKey        | string | true |
-| oauthSignature| string | true |
 | accessToken   | string | true |
-| userId        | string | true |
--- node_helper.js
+++ node_helper.js
@@ -6,15 +6,6 @@ const request = require('request');
 
 const agent = request.defaults({ json: true });
 
-const randomString = (length) => {
-  let text = '';
-  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for (let i = 0; i < length; i++) {
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-}
-
 const generateQuery = (params) => 
   Object.keys(params)
     .map((key) => key + '=' + params[key])
@@ -26,10 +17,7 @@ const measureTypes = {
 
 /**
  * @typedef {Object} Config
- * @property {string} apiKey
- * @property {string} oauthSignature
  * @property {string} accessToken
- * @property {string} userId
  */
 
 /**
@@ -37,40 +25,20 @@ const measureTypes = {
  * @param {Config} config 
  */
 const createApiUrl = (config) => {
-  const timestamp = Math.round(Date.now() / 1000);
-  const nonce = randomString(32);
   const queryParams = generateQuery({
     action: 'getmeas',
-    oauth_consumer_key: config.apiKey,
-    oauth_nonce: nonce,
-    oauth_signature: config.oauthSignature,
-    oauth_signature_method: 'HMAC-SHA1',
-    oauth_timestamp: timestamp,
-    oauth_token: config.accessToken,
-    oauth_version: '1.0',
-    userid: config.userId,
+    access_token: config.accessToken,
     meastype: measureTypes.weight,
-    limit: 7,
   });
-  return `http://api.health.nokia.com/measure?${queryParams}`;
+  return `https://wbsapi.withings.net/measure?${queryParams}`;
 }
 
 const isWeightType = measure => measure.type === measureTypes.weight;
 
-/**
- * 
- * @param {Partial<Config>} config 
- */
-const isValidConfig = (config) => {
-  const configKeys = Object.keys(config);
-  const requiredKeys = ['apiKey', 'oauthSignature', 'accessToken', 'userId'];
-  return requiredKeys.every(requiredKey => configKeys.includes(requiredKey));
-}
-
 module.exports = NodeHelper.create({
   socketNotificationReceived: function(notification, payload) {
     if (notification === 'MMM_WITHINGS_START') {
-      if (isValidConfig(payload)) {
+      if (payload.accessToken) {
         this.checkLatestWeight(payload);
         setInterval(() => {
           this.checkLatestWeight(payload);
@@ -91,7 +59,7 @@ module.exports = NodeHelper.create({
       if (error) {
         console.error('Something went wrong with Withings call', error);
         this.sendSocketNotification('ERROR', 'Something went wrong. Are you connected to Internet?');
-      } else if (body.statusCode === 200) {
+      } else if (body.status === 0) {
         const weights = body.body.measuregrps.map(measuregrp => {
           const measure = measuregrp.measures.find(isWeightType);
           return measure.value * Math.pow(10, measure.unit);
